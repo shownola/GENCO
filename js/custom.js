b82eec32009932eb9ae70486400b7ce98aee2049
shownola <swasieko@gmail.com>
2019-10-08 13:54:10 -0700

    styled landing page

--- js/custom.js
+++ js/custom.js
@@ -6,5 +6,41 @@ $(document).ready(function(){
     } else {
       $('.navbar').removeClass('solid');
     }
+  });
+});
+
+/* CLOSE MOBILE NAV ON CLICK */
+
+$(document).ready(function(){
+  $(document).click(function(event){
+    var clickover = $(event.target);
+    var _opened = $(".navbar-collapse").hasClass("show");
+    if(_opened === true && !clickover.hasClass(".navbar-toggler")){
+      $(".navbar-toggler").click();
+    }
   })
 })
+
+/* SMOOTH SCROLLING TO LINKS */
+
+$(document).ready(function(){
+  $("a").on("click", function(event) {
+    if(this.hash !== ""){
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } // End of if statement
+  });
+});
+
+/* BOUNCING DOWN ARROW */
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
+});
