3b3020bcc4badd235fb2ec6f4bc34bc0d1fa08b4
shownola <swasieko@gmail.com>
2019-10-11 13:31:28 -0700

    complete team and skills section

--- js/custom.js
+++ js/custom.js
@@ -44,3 +44,36 @@ $(document).ready(function(){
     $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250);
   });
 });
+
+/* MEET THE TEAM */
+
+$(document).ready(function(){
+  $('#team-slider').owlCarousel({
+    items:3,
+    autoplay:true,
+    smartSpeed:700,
+    loop:true,
+    autoplayHoverPause:true,
+    responsive : {
+      0 : {
+        items: 1
+      },
+      576 : {
+        items: 2
+      },
+      768 : {
+        items: 3
+      }
+    }
+  }
+  );
+});
+
+/*  SKILLS COUNTER */
+
+$(document).ready(function(){
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1800
+  })
+})
